3ffe600cad26f1cca15611692f2d417acc9562e9
jsjiang <jing.jiang@ucop.edu>
2026-07-21 16:19:04 -0700

    update comments

--- tests/test_service.py
+++ tests/test_service.py
@@ -1,7 +1,5 @@
-"""Test cases for CORS support by the web application.
+"""Test cases for ARKS.
 
-Liberal CORS support is needed to enable in-browser programmatic use of
-PID identified resources.
 """
 
 import logging
@@ -51,7 +49,12 @@ def arksapp():
     # cleanup, remove the config db
     os.unlink(CONFIG_DB)
 
+"""Test cases for CORS support by the web application.
 
+Liberal CORS support is needed to enable in-browser programmatic use of
+PID identified resources.
+Disable cors tests for now.
+"""
 def x_test_cors_headers(arksapp):
     client = fastapi.testclient.TestClient(arksapp, follow_redirects=False)
     headers = {
@@ -63,6 +66,17 @@ def x_test_cors_headers(arksapp):
     response = client.request("GET", "/")
     assert response.headers.get("access-control-allow-origin", None) is None
 
+"""Test cases for ARK resolution.
+Sample records are taken from "Testing an ARK resolver instance"
+https://github.com/CDLUC3/arksorg-site/blob/main/doc_src/operation_test.md
+
+ark:/99166/w6sr6tn8:
+ark:/65665/3f9748e2c-affd-44ee-9c14-4eb966e2955c: 
+ark:27023/829db79be004882891dd7b88c2ea6236: 
+ark:67531/metadc3211:
+ark:69774/rgm2020: NLID substitution
+ark:19156/tkt42/03n01: suffix passthrough
+"""
 
 info_cases = (
     ("ark:/99166/w6sr6tn8", {"uniq": "ark:99166/w6", "target": "http://socialarchive.iath.virginia.edu/ark:/${content}", "http_code": 303}),
@@ -71,6 +85,15 @@ info_cases = (
     ("ark:67531/metadc3211", {"uniq": "ark:67531", "target": "http://digital.library.unt.edu/ark:/${content}", "http_code": 302}),
     ("ark:69774/rgm2020", {"uniq": "ark:69774", "target": "https://hdlab.space/ark/${value}", "http_code": 302}),
     ("ark:19156/tkt42/03n01", {"uniq": "ark:19156/tkt42", "target": "https://vocab.participatory-archives.ch/vocab.participatory-archives.ch/brunner${suffix}", "http_code": 302}),
+)
+
+resolve_cases = (
+    ("ark:/99166/w6sr6tn8", {"location":"http://socialarchive.iath.virginia.edu/ark:/99166/w6sr6tn8", "status": 303}),
+    ("ark:/65665/3f9748e2c-affd-44ee-9c14-4eb966e2955c", {"location":"https://ezid.cdlib.org/ark:/65665/3f9748e2caffd44ee9c144eb966e2955c", "status": 302}),
+    ("ark:27023/829db79be004882891dd7b88c2ea6236", {"location":"https://id.colonialcollections.nl/ark:/27023/829db79be004882891dd7b88c2ea6236", "status": 302}),
+    ("ark:67531/metadc3211", {"location":"http://digital.library.unt.edu/ark:/67531/metadc3211", "status": 302}),
+    ("ark:69774/rgm2020", {"location":"https://hdlab.space/ark/rgm2020", "status": 302}),
+    ("ark:19156/tkt42/03n01", {"location":"https://vocab.participatory-archives.ch/vocab.participatory-archives.ch/brunner/03n01", "status": 302}),
 )
 
 @pytest.mark.parametrize("test,expected", info_cases)
@@ -83,16 +106,6 @@ def test_info_schemes(arksapp, test, expected):
     assert _match.get("definition", {}).get("target", None) == expected["target"]
     assert _match.get("definition", {}).get("http_code", None) == expected["http_code"]
     assert response.status_code == 200
-
-
-resolve_cases = (
-    ("ark:/99166/w6sr6tn8", {"location":"http://socialarchive.iath.virginia.edu/ark:/99166/w6sr6tn8", "status": 303}),
-    ("ark:/65665/3f9748e2c-affd-44ee-9c14-4eb966e2955c", {"location":"https://ezid.cdlib.org/ark:/65665/3f9748e2caffd44ee9c144eb966e2955c", "status": 302}),
-    ("ark:27023/829db79be004882891dd7b88c2ea6236", {"location":"https://id.colonialcollections.nl/ark:/27023/829db79be004882891dd7b88c2ea6236", "status": 302}),
-    ("ark:67531/metadc3211", {"location":"http://digital.library.unt.edu/ark:/67531/metadc3211", "status": 302}),
-    ("ark:69774/rgm2020", {"location":"https://hdlab.space/ark/rgm2020", "status": 302}),
-    ("ark:19156/tkt42/03n01", {"location":"https://vocab.participatory-archives.ch/vocab.participatory-archives.ch/brunner/03n01", "status": 302}),
-)
 
 @pytest.mark.parametrize("test,expected", resolve_cases)
 def test_resolve_schemes1(arksapp, test, expected):
